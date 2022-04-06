acaa6eb5d54b9f4a83006e2d6f03a22bb86414ed
Alex Maxim <81637630+alexmaxim28@users.noreply.github.com>
2022-04-06 22:06:49 +0300

    Delete tema_6/DTO_DAO_Practice-master directory

--- tema_6/DTO_DAO_Practice-master/src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.dao.OrderDAO;
-import com.example.demo.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository <OrderDAO, Integer> {
-}
